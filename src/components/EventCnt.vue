42d96efeae882fe9991995a090ad38b51f55f4df
Simon@Six <simon@madebysix.com>
2024-05-22 00:35:51 +0100

    buy tikcets link font size increase

--- src/components/EventCnt.vue
+++ src/components/EventCnt.vue
@@ -2,24 +2,19 @@
   <div class="event">
 
     <div v-if="props.event.tickets_link">
-
       <div>
-        <a class="uppercase font-bold text-black	" :href="props.event.tickets_link" target="_blank">
+        <a class="text-xl uppercase font-bold text-black" :href="props.event.tickets_link" target="_blank">
           {{ props.event.buy_tickets_text }}
         </a>
       </div>
 
-
       <a :href="props.event.tickets_link" target="_blank">
         <img :src="props.event.image.path" />
       </a>
-
     </div>
 
     <div v-else>
-      
       <img :src="props.event.image.path" />
-
     </div>
 
   </div>
